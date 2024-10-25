b80722990dfd30b6018928b70c46348031b55e34
Chris Victorio <chrisdvictorio@gmail.com>
2024-10-25 20:45:51 +0800

    Project Completed

--- backend/server.js
+++ backend/server.js
@@ -33,10 +33,10 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
 
